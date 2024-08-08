62f7c500d6fb244aced2cbbd50a505946721da65
Max-Kucher <mkuchirov@gmail.com>
2024-08-08 11:29:25 +0300

    Added new favicon and other meta tags.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,6 +6,19 @@ export default defineNuxtConfig({
       name: 'page',
       mode: 'out-in',
     },
+    head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      ],
+      meta: [
+        { name: 'msapplication-TileColor', content: '#7c613a' },
+        { name: 'theme-color', content: '#000000' }
+      ]
+    },
   },
   experimental: {
     viewTransition: true,
