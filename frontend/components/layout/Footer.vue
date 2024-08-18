d1ceb82b508544b403e313e83ac7ef547907a362
Max-Kucher <mkuchirov@gmail.com>
2024-08-18 15:28:11 +0300

    Added breakpoint for small tablets

--- frontend/components/layout/Footer.vue
+++ frontend/components/layout/Footer.vue
@@ -6,16 +6,16 @@ const buttonsJoinCommunityClass = 'text-black lg:ml-auto w-max'
 
 <template>
   <footer class="bg-black text-white">
-    <LayoutContainer class="max-tablet:pt-12 max-tablet:pb-16 tablet:max-lg:pt-16 tablet:max-lg:pb-24 lg:pt-24 lg:pb-36 app-layout-cols">
-      <div class="flex flex-wrap gap-x-6 justify-between tablet:block pt-2 lg:pt-5 space-y-6">
-        <AppLogo class="w-full tablet:mb-10" />
+    <LayoutContainer class="max-small-tablet:pt-12 max-small-tablet:pb-16 small-tablet:max-lg:pt-16 small-tablet:max-lg:pb-24 lg:pt-24 lg:pb-36 app-layout-cols">
+      <div class="flex flex-wrap gap-x-6 justify-between small-tablet:block pt-2 lg:pt-5 space-y-6">
+        <AppLogo class="w-full small-tablet:mb-10" />
 
         <LayoutFooterSocials />
 
         <ButtonsJoinCommunity :class="twMerge(buttonsJoinCommunityClass, 'lg:hidden')" />
       </div>
 
-      <div class="mt-10 tablet:mt-0 tablet:max-lg:col-span-2 lg:col-span-1">
+      <div class="mt-10 small-tablet:mt-0 small-tablet:max-lg:col-span-2 lg:col-span-1">
         <AppFooterMenu />
       </div>
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -24,6 +24,7 @@ export default {
       },
       screens: {
         'tablet': '834px',
+        'small-tablet': '700px',
       },
     },
   },
